104e17ae516a0d8e0c93cea258b8eca7c0971985
MrSwoops <rmcooke33@gmail.com>
2025-10-18 00:46:55 -0400

    updates

--- js/pages/projects.js
+++ js/pages/projects.js
@@ -1,12 +1,16 @@
 const projectGroups = [
     [
-        { project: "Kleine", repo: "./klein.html", 
-        image: "./Images/kleine/Thumb.JPG",
-        role: "Programmer/3D Art", team: "DoughyWorks" },
+        // { project: "Project Hola", repo: "./hola.html", 
+        // image: "./Images/blog/HamsterBeef.PNG",
+        // role: "Lead", team: "Solo Study" },
 
         { project: "Tanks", repo: "./ai-study.html", 
         image: "./Images/tanks/AStar.gif",
-        role: "Programming/Design", team: "AI Study" },
+        role: "Programming/Design", team: "Solo Study" },
+
+        { project: "Souls-Likes", repo: "./souls-like-study.html", 
+        image: "",
+        role: "Programming/Design", team: "Solo Study" },
 
         //{ project: "Bullet Hell", repo: "", 
         //image: "",
@@ -15,13 +19,17 @@ const projectGroups = [
         
     ],
     [
+        // { project: "FleetCom", repo: "./fleetcom.html", 
+        // image: "./Images/blog/Megastruct.png",
+        // role: "Project & Programming Lead", team: "Team FleetCom" },
+
+        { project: "Kleine", repo: "./klein.html", 
+        image: "./Images/kleine/Thumb.JPG",
+        role: "Programmer/3D Art", team: "DoughyWorks" },
+
         { project: "Asphodel Files", repo: "./psychomanteum.html", 
         image: "./Images/psychomanteum/Bar.JPG",
         role: "Lead Programmer/Level Design", team: "Team Psychomanteum" },
-
-        { project: "Souls-Likes", repo: "./souls-like-study.html", 
-        image: "",
-        role: "Programming/Design", team: "Study" },
 
         //{ project: "Meltdown", repo: "https://www.google.com/", 
         //image: "",
--- js/pages/psychomanteum.js
+++ js/pages/psychomanteum.js
@@ -1,14 +1,4 @@
-const pageInfo = {
-    projectTitle: "Asphodel Files",
-    subTitle: "",
-    video: {src: "./Videos/Asphodel_Files_Trailer.mp4", type: "mp4"},
-    about: "About the project....",
-    features: ["Branching Dialogue", "Item Inspection", "Save/Load"],
-    reflection: "Reflection on the project....",
-    links: [{link: "https://github.com/TippyToo/psychomanteum", text: "GitHub"}, 
-        {link: "https://tippytoo.itch.io/asphodel-files", text: "Itch.io"}]
 
-}
 
 
 window.onload = function () 
